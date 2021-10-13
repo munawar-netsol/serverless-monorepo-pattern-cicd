9d54d635c839e29230ed267c529f58134885050c
munawar-netsol <munawersheikh@gmail.com>
2021-10-14 04:56:28 +0500

    First Commit

---
+++ service1/getUser.js
@@ -0,0 +1,20 @@
+const Responses = require('./API_Responses');
+
+exports.handler = async event => {
+    console.log('event', event);    
+    if (!event.pathParameters || !event.pathParameters.ID) {
+        return Responses._400({message: 'Missing the ID from Path!'});
+    }
+    let ID = event.pathParameters.ID;
+    if (data[ID]) {
+        // return data
+        return Responses._200(data[ID]);
+    }
+
+    return Responses._400 ({message: 'No ID in Data'});
+    // Failed as ID not in data.
+}
+
+const data = {
+    1: { name: 'Munawar', age: 25, job: 'Journalist' }
+};
